406e384dfb5e58c550e3137984988594fd9436bb
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-30 15:00:24 -0300

    iniciando e finalizando requisitos 1 e 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+list_file = []
+
+
 def txt_importer(path_file):
-    raise NotImplementedError
+    if path_file[-4::] != ".txt":
+        print("Formato inválido")
+        return "Formato inválido"
+    try:
+        with open(path_file, 'r') as f:
+            lines = f.readlines()
+            f.close()
+            list_file.append(lines)
+            return list_file
+    except Exception:
+        print(f"Arquivo {path_file} não encontrado")
+
+
+# txt_importer('statics/arquivo_teste.txt')
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from file_management import txt_importer
+
+
 def process(path_file):
-    raise NotImplementedError
+    get_read_file = txt_importer(path_file)
+    if not isinstance(get_read_file, list):
+        return get_read_file
+
+    file_name = path_file.split("/")
+
+    process_file = {
+        "nome_do_arquivo": file_name[len(file_name) - 1],
+        "qtd_linhas": len(get_read_file[0]),
+        "posicao": len(get_read_file)
+    }
+
+    print(process_file)
+
+    return process_file
 
 
 def remove():
@@ -8,3 +25,6 @@ def remove():
 
 def file_metadata(position):
     raise NotImplementedError
+
+
+process('statics/arquivo_teste.txt')
