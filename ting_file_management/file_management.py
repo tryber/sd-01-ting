fd7c4ea70b511ebd397f143ec7ca9fa05e8e6c3e
conradomedeirosdev <conradodemedeiros@hotmail.com>
2020-10-07 16:55:01 -0300

    projeto finalizado ufa! valeu doug

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
-def txt_importer(path_file):
-    raise NotImplementedError
+from services.manager import Manager
+
+# Projeto feito em pair programer com doug funny
+
+
+class FileManagement:
+    def __init__(self):
+        self.fifo = []
+
+    def txt_importe(self, path_file):
+        main = Manager()
+        if not main.valid_file_checker(path_file):
+            return None
+        value = main.file_reader_writer("reader", path_file)
+        self.fifo.append(value)
+        print("importado com sucesso!")
